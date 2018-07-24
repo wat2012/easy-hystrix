44b2d55eb09c4538844d744f64c46ea621f9945a
Jerome Chang <cxh01987@163.com>
2018-07-24 21:23:09 +0800

    implement fallback with hystrix

---
+++ src/main/java/com/xtechstack/easy/hystrix/HystrixApplication.java
@@ -0,0 +1,46 @@
+package com.xtechstack.easy.hystrix;
+
+import java.util.Map;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@RestController
+public class HystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixApplication.class, args);
+	}
+
+	@Component
+	public class HelloIntegration {
+		@HystrixCommand(fallbackMethod = "fallbackHello")
+		public String hello(String name) throws Exception {
+			if(name.equals("xtechstack")){
+				throw new Exception("xtechstack");
+			}
+			return "Hello, "+name+"!";
+		}
+
+		public String fallbackHello(String name) {
+			return "blog.xtechstack.com";
+		}
+	}
+	@Autowired
+	private HelloIntegration hi;
+
+	@GetMapping("/hello")
+	public String home(@RequestParam("name") String name) throws Exception{
+		return hi.hello(name);
+	}
+}
