362ba88dda4b11fec1e690ff8ff6949322585947
Jerome Chang <cxh01987@163.com>
2018-07-24 21:35:00 +0800

    add hystrix dashboard support

--- src/main/java/com/xtechstack/easy/hystrix/HystrixApplication.java
+++ src/main/java/com/xtechstack/easy/hystrix/HystrixApplication.java
@@ -1,13 +1,12 @@
 package com.xtechstack.easy.hystrix;
 
-import java.util.Map;
-
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @RestController
 public class HystrixApplication {
 
